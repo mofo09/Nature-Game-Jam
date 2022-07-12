be358fc6a527ec0a0ebfc22f93be34549f5db227
Jose Ferrusca <EndureTheGame@users.noreply.github.com>
2022-07-11 22:53:37 -0500

    Early Version
    I'm having trouble when I upload the script with player movement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float senX;
+    public float senY;
+
+    public Transform orientacion;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientacion.rotation = Quaternion.Euler(0, yRotation, 0);
+
     }
-}
+
+ }
