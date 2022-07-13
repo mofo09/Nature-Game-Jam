5fa70656e0d5fd86fd80e7acf744c0b9463d699c
Jose Ferrusca <EndureTheGame@users.noreply.github.com>
2022-07-12 20:40:58 -0500

    URP Update
    Project has been updated to use URP. Please be ready to port any progress as by pulling main branch will override any work before the commit.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float movementSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+    
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    { 
+        MyInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * movementSpeed * 10.0f, ForceMode.Force);
+    }
+}
