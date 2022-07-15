5ec51f3b5d559b742e812d5d5a87cb2fa9516cf9
Jose Ferrusca <EndureTheGame@users.noreply.github.com>
2022-07-15 16:32:15 -0500

    Basic Ai done for Enemy
    Ai parameter can be adjusted Detection distance & Attack distance. Attack state temporary.

---
+++ Assets/Scripts/ScriptableObjects/EnemySO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "EnemySO", menuName = "ScriptableObjects/Enemy01")]
+public class EnemySO : ScriptableObject
+{
+  
+}
