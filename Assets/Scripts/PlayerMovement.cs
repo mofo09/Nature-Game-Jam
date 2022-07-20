9ac030555b0ceb4be04cabe16c813c4909454cd4
Knappkin <44062671+Knappkin@users.noreply.github.com>
2022-07-20 00:38:39 -0400

    Revert "Merge branch 'EndureTheGame:main' into Knapp-Branch"
    This reverts commit a50f834fcda0cbab4b3b222f7ea35f4d7e6a66ba, reversing
    changes made to 405fc226d45ea1423fa3e0b25c7ac7dd5f3e7da8.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float health = 100;
     public float speed = 12.0f;
     public float gravity = -9.81f;
     public float jump = 3.0f;
@@ -40,16 +39,4 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0) Invoke(nameof(DestroyPlayer), 0.5f);
-    }
-    private void DestroyPlayer()
-    {
-        Destroy(gameObject);
-    }
-
 }
